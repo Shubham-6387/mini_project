9e720a6e2e833b3c2d8835956dc863453e559b30
Abhishek Sutar <sutarabhishek83@gmail.com>
2025-11-27 20:42:01 +0530

    Create .env

--- public/firebase.js
+++ public/firebase.js
@@ -1,5 +1,7 @@
 // public/firebase.js
 import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -14,6 +16,15 @@ import {
   getDoc,
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-firestore.js";
+
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+//   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+//   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: pimport.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: import.meta.env.VITE_FIREBASE_APP_ID
+// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyAaj0u3TeB3UCjveXUPF4gN0ErthptwKlc",
